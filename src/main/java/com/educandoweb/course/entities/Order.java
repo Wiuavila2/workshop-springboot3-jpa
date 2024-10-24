973c9a2ea36d3d4c2b69355978356599573c15ea
WiuAvila2 <wiuneivetom.lisboa@outlook.com>
2024-10-24 15:47:54 -0300

    enum de status de pedido

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -19,6 +20,8 @@ public class Order implements Serializable {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT" )
     private Instant moment;
+
+    private Integer orderStatus;
 //joinColumn com o nome da chave estrangeira;
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -28,10 +31,11 @@ public class Order implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus , User client) {
         this.id = id;
         this.moment = moment;
-        Client = client;
+        setOrderStatus(orderStatus);
+        this.Client = client;
     }
 
     public Long getId() {
@@ -56,6 +60,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         Client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
