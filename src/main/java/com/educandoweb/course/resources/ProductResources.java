981e2c3a4252ca9297954fe8ed9bd4ef8197b296
WiuAvila2 <wiuneivetom.lisboa@outlook.com>
2024-10-25 16:49:04 -0300

    associação de produtos e categorias

--- src/main/java/com/educandoweb/course/resources/ProductResources.java
+++ src/main/java/com/educandoweb/course/resources/ProductResources.java
@@ -1,7 +1,7 @@
 package com.educandoweb.course.resources;
 
-import com.educandoweb.course.entities.Category;
-import com.educandoweb.course.services.CategoryServices;
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.services.ProductServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,21 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/categories")
-public class CategoryResources {
+@RequestMapping(value = "/products")
+public class ProductResources {
 
     @Autowired
-    private CategoryServices service;
+    private ProductServices service;
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll(){
-        List<Category> list = service.findAll();
+    public ResponseEntity<List<Product>> findAll(){
+        List<Product> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Category> findById (@PathVariable Long id){
-        Category obj = service.findById(id);
+    public ResponseEntity<Product> findById (@PathVariable Long id){
+        Product obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 }
