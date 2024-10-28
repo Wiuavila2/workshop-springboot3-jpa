4bb44759bdc242556babbf0bcd1f5725f59aedf1
WiuAvila2 <wiuneivetom.lisboa@outlook.com>
2024-10-28 16:13:42 -0300

    exceção de delete

--- src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.resources.exceptions;
 
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -19,5 +20,13 @@ public class ResourceExceptionHandler {
         StandardError err = new StandardError(Instant.now(), request.getRequestURI(), error, e.getMessage(), status.value());
         return ResponseEntity.status(status).body(err);
                 //.status para que seja personalizado e aceite o status definido
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database (DatabaseException e , HttpServletRequest request){
+        String error = "Database Error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), request.getRequestURI(), error, e.getMessage(), status.value());
+        return ResponseEntity.status(status).body(err);
     }
 }
